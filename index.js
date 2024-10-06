ba2081c23d05d27f5dce140735bd663b3ed47b29
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2024-10-06 22:30:39 +0700

    feat: first iteration of home page, moving previous home page to contact page

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 const form = document.getElementById('new-contact');
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+if(form) {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        const firstName = document.getElementById('fname').value;
+        const lastName = document.getElementById('lname').value;
+        const company = document.getElementById('company').value;
+        const jobTitle = document.getElementById('job-title').value;
+        const phone = document.getElementById('phone').value;
+        const dateBirth = document.getElementById('date-birth').value;
+        const monthBirth = document.getElementById('month-birth').value;
+        const yearBirth = document.getElementById('year-birth').value;
+        const birthDate = dateBirth + '-' + monthBirth + '-' + yearBirth;
+    
+        const newContact = {
+            firstName: firstName,
+            lastName: lastName,
+            company: company,
+            jobTitle: jobTitle,
+            phone: phone,
+            birthDate: birthDate
+        };
+    
+        localStorage.setItem('newContact', JSON.stringify(newContact));
+    
+        alert('Contact saved, please check on your local storage');
+    });
+}
 
-    const firstName = document.getElementById('fname').value;
-    const lastName = document.getElementById('lname').value;
-    const company = document.getElementById('company').value;
-    const jobTitle = document.getElementById('job-title').value;
-    const phone = document.getElementById('phone').value;
-    const dateBirth = document.getElementById('date-birth').value;
-    const monthBirth = document.getElementById('month-birth').value;
-    const yearBirth = document.getElementById('year-birth').value;
-    const birthDate = dateBirth + '-' + monthBirth + '-' + yearBirth;
+const hamburger = document.getElementById("hamburger");
+const navBar = document.getElementById("nav-bar");
+const closeNav = document.getElementById("close-nav");
+const contact = document.getElementById("contact");
 
-    const newContact = {
-        firstName: firstName,
-        lastName: lastName,
-        company: company,
-        jobTitle: jobTitle,
-        phone: phone,
-        birthDate: birthDate
-    };
-
-    localStorage.setItem('newContact', JSON.stringify(newContact));
-
-    alert('Contact saved, please check on your local storage');
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hidden");
+    navBar.classList.remove("hidden");
+    closeNav.classList.remove("hidden");
+    contact.classList.remove("main-absolute");
 });
+
+closeNav.addEventListener("click", () => {
+    hamburger.classList.toggle("hidden");
+    navBar.classList.add("hidden");
+    closeNav.classList.add("hidden");
+    contact.classList.add("main-absolute");
+})
