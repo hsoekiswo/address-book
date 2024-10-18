bae5355b8ee541a5e2aa27319986578bf744e4b9
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2024-10-19 04:20:10 +0700

    refactor: adopt react framework

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+import Nav from './Nav';
+
+
+function App() {
+
+    return (
+        <div id="container">
+            <Nav/>
+        </div>
+    );
+}
+
+export default App;
